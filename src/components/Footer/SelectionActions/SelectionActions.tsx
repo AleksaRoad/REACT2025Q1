4f347b6baace08be413cf38d67ec4981fc647b11
AleksaRoad <aleksaroad@gmail.com>
2025-02-18 17:36:20 +0300

    fix: change animation for SelectionActions component and adjust positioning to prevent overlap with pagination buttons

--- src/components/Footer/SelectionActions/SelectionActions.tsx
+++ src/components/Footer/SelectionActions/SelectionActions.tsx
@@ -1,13 +1,19 @@
+import clsx from 'clsx';
+import { useState, useEffect, type FC } from 'react';
 import { useDispatch } from 'react-redux';
 
-import { BUTTON_STYLES, CACHE_KEY, useStorage } from '@/shared';
-import { useAppSelector } from '@/shared/hooks/useAppSelector';
+import { BUTTON_STYLES, CACHE_KEY, useStorage, useAppSelector } from '@/shared';
 import { clearFavorites } from '@/store';
 
-export const SelectionActions = () => {
+export const SelectionActions: FC = () => {
   const favorites = useAppSelector((state) => state.favorites);
   const dispatch = useDispatch();
   const { save } = useStorage(CACHE_KEY.favorites);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(favorites.length > 0);
+  }, [favorites]);
 
   const handleDeselect = () => {
     dispatch(clearFavorites());
@@ -15,9 +21,18 @@ export const SelectionActions = () => {
   };
 
   return (
-    <div className="dark:bg-blue-xs/80 flex items-center justify-between gap-5 rounded-3xl bg-amber-300/70 px-10 py-6">
+    <div
+      className={clsx(
+        'dark:bg-blue-xs relative mx-auto flex w-full max-w-3xl items-center gap-3 rounded-2xl bg-amber-300/80 p-4 transition-opacity duration-500',
+        {
+          'opacity-0': !isVisible,
+          'opacity-100': isVisible,
+        }
+      )}
+      style={{ zIndex: 10 }}
+    >
       <button className={BUTTON_STYLES.favorites} onClick={handleDeselect}>
-        Deselect
+        Unselect all
       </button>
       <span>{favorites.length} items in favorites</span>
       <button className={BUTTON_STYLES.favorites}>Download</button>
