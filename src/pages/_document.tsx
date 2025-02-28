2d36cab6f8b3de8c08a0d960cad7090b68c20509
AleksaRoad <aleksaroad@gmail.com>
2025-02-28 13:33:04 +0300

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,17 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <link rel="icon" href="/favicon/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.webp"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
